ab2a93651b77b40f836fe9e04725bef5b6321e3e
alty <timoxa18rus@gmail.com>
2025-03-23 07:50:22 +0400

    n

--- deepra/deepra/settings.py
+++ deepra/deepra/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_app',
+    'deepra.hello_app',
 ]
 
 MIDDLEWARE = [
